170e519b73fffee71c0423723bd9f01429ac9e33
ermekbaeva <ermekbaevameerim@gmail.com>
2021-03-18 20:58:30 +0300

    add new locators

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,12 @@ class MainPageLocators():
 class LoginPageLocators():
     REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, '#add_to_basket_form')
+    BASKET_PRICE = (By.CSS_SELECTOR,'div[id="messages"]>div>div>p>strong')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, 'div.col-sm-6.product_main>p.price_color')
+    BASKET_BOOK_NAME = (By.CSS_SELECTOR, 'div.alert.alert-safe.alert-noicon:first-child')
+    BOOK_NAME = (By.CSS_SELECTOR, 'div.col-sm-6.product_main>h1')
+
+
