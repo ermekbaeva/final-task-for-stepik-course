7c83c3d33dfa1de7f8d637a7e9724671ecf3aa3b
ermekbaeva <ermekbaevameerim@gmail.com>
2021-03-20 01:18:58 +0300

    add tests to check going to login from product page

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,7 @@ import time
 @pytest.mark.skip
 @pytest.mark.parametrize('promo',
                          ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
-def test_guest_can_add_product_to_basket(browser,promo):
+def test_guest_can_add_product_to_basket(browser, promo):
     #  link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}'
     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=offer0'
     page = ProductPage(browser, link)
@@ -17,6 +17,7 @@ def test_guest_can_add_product_to_basket(browser,promo):
     page.solve_quiz_and_get_code()
     page.basket_price_should_be_correct()
     page.book_name_in_basket_should_be_correct()
+
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -33,6 +34,7 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
+
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
@@ -41,11 +43,13 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.should_be_disappeared()
 
+
 def test_guest_should_see_login_link_on_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
