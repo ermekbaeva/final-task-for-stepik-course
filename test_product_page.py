94a5e23d782e0e29e6f43b848743f4c6ad08f462
ermekbaeva <ermekbaevameerim@gmail.com>
2021-03-19 15:36:07 +0300

    add parametrize, promo number 7 - xfail

--- test_product_page.py
+++ test_product_page.py
@@ -3,7 +3,9 @@ from pages.base_page import BasePage
 import pytest
 import time
 
-@pytest.mark.parametrize('promo', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+
+@pytest.mark.parametrize('promo',
+                         ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, promo):
     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}'
     page = ProductPage(browser, link)
