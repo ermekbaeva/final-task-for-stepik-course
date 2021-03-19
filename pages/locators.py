d319098134890b30b4ebc6890cdfda5de54fabb2
ermekbaeva <ermekbaevameerim@gmail.com>
2021-03-20 01:15:58 +0300

    add new selectors - success message

--- pages/locators.py
+++ pages/locators.py
@@ -11,7 +11,11 @@ class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, '#add_to_basket_form')
     BASKET_PRICE = (By.CSS_SELECTOR,'div[id="messages"]>div>div>p>strong')
     PRODUCT_PRICE = (By.CSS_SELECTOR, 'div.col-sm-6.product_main>p.price_color')
-    BASKET_BOOK_NAME = (By.CSS_SELECTOR, 'div.alert.alert-safe.alert-noicon:first-child')
+    BASKET_BOOK_NAME = (By.CSS_SELECTOR, 'div.alert.alert-safe.alert-noicon:first-child>div>strong')
     BOOK_NAME = (By.CSS_SELECTOR, 'div.col-sm-6.product_main>h1')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, 'div.alert.alert-safe.alert-noicon:first-child')
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, '#login_link_inc')
 
