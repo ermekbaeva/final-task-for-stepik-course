15b95d707565149f77f58a0044c474486b792fab
ermekbaeva <ermekbaevameerim@gmail.com>
2021-03-22 17:07:26 +0300

    add new locators

--- pages/locators.py
+++ pages/locators.py
@@ -18,4 +18,7 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, '#login_link_inc')
+    BASKET_LINK = (By.CSS_SELECTOR, 'span.btn-group')
+    BASKET_EMPTY_MESSAGE = (By.CSS_SELECTOR, '#content_inner>p')
+    PRODUCTS_IN_BASKET = (By.CSS_SELECTOR, 'div.basket-title.hidden-xs')
 
