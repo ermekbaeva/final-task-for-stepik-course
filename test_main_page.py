59e7d6612bbb703b27ad2d57d620174295a57caf
ermekbaeva <ermekbaevameerim@gmail.com>
2021-03-22 17:12:47 +0300

    add test for check basket

--- test_main_page.py
+++ test_main_page.py
@@ -1,23 +1,29 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
+from pages.base_page import BasePage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляо драйвера и url адрес
+    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляо драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
-
+    page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
 
-def test_guest_should_be_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
-    page = LoginPage(browser, link)
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = BasketPage(browser, link)
     page.open()
-    page.should_be_login_page()
+    page.go_to_basket()
+    page.should_not_be_products_in_basket()
+    page.should_be_message_basket_empty()
+
+
 
